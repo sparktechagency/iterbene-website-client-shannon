0c3688a020389c1ea84d4b4ecb0392da851d884a
Md Rakib Ali <rakib2020.tkg@gmail.com>
2025-04-13 15:18:45 +0600

    feat: implement UpcomingEventCard component and integrate it into UpcomingEvents page

--- src/components/pages/events/upComingEvents/upComing-event-card.tsx
+++ src/components/pages/events/upComingEvents/upComing-event-card.tsx
@@ -1,9 +1,73 @@
-import React from 'react'
-
-const UpcomingEventCard = () => {
-  return (
-    <div>UpcomingEventCard</div>
-  )
+import Image from "next/image";
+import React from "react";
+import { PiUserBold } from "react-icons/pi";
+interface IAuthor {
+  fullName: string;
+  profileImage: {
+    imageUrl: string;
+  };
 }
 
-export default UpcomingEventCard
+interface IEvent {
+  id: number;
+  title: string;
+  author: IAuthor;
+  image: {
+    imageUrl: string;
+  };
+  members: string;
+}
+
+interface UpcomingEventCardProps {
+  event: IEvent;
+}
+
+const UpcomingEventCard: React.FC<UpcomingEventCardProps> = ({ event }) => {
+  return (
+    <div className="w-full bg-white rounded-2xl  p-4 flex flex-col items-center">
+      {/* Group Image */}
+      <div className="w-full h-[350px] bg-gray-200 rounded-xl mb-4 relative">
+        <Image
+          src={event.image.imageUrl}
+          alt={event.title}
+          width={350}
+          height={350}
+          className="w-full h-full object-cover rounded-2xl mb-4"
+        />
+        <div className="absolute px-4 py-5 rounded-lg top-0 left-0 right-0 bottom-0 bg-gray-950/20">
+          <div className="w-full h-full flex flex-col justify-between">
+            <div className="flex  justify-between items-center">
+              <Image
+                src={event?.author?.profileImage?.imageUrl || ""}
+                alt={event.author.fullName}
+                width={60}
+                height={60}
+                className="size-[60px] rounded-full object-cover mr-3 "
+              />
+              <div className="bg-white rounded-full px-4 py-2 flex items-center gap-1">
+                <PiUserBold size={24} className="text-secondary" />
+                <span className="text-sm font-semibold text-gray-800">
+                  {event.members}
+                </span>
+              </div>
+            </div>
+            <h2 className="text-2xl md:text-[32px] font-semibold text-white">
+              {event.title}
+            </h2>
+          </div>
+        </div>
+      </div>
+      {/* Buttons */}
+      <div className="flex flex-col gap-3 w-full">
+        <button className="bg-secondary hover:bg-[#FEEFE8] hover:text-secondary text-white  px-5 py-3.5 rounded-lg border border-secondary transition cursor-pointer">
+          View
+        </button>
+        <button className="border border-[#9EA1B3] text-gray-900 px-5 py-3.5   rounded-lg hover:bg-gray-100 transition cursor-pointer">
+          Remove
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default UpcomingEventCard;
--- src/components/pages/events/upComingEvents/upComing-events.tsx
+++ src/components/pages/events/upComingEvents/upComing-events.tsx
@@ -1,9 +1,90 @@
-import React from 'react'
-
-const UpcomingEvents = () => {
-  return (
-    <div>UpcomingEvents</div>
-  )
+import React from "react";
+import UpcomingEventCard from "./upComing-event-card";
+interface IAuthor {
+  fullName: string;
+  profileImage: {
+    imageUrl: string;
+  };
 }
 
-export default UpcomingEvents
+interface IEvent {
+  id: number;
+  title: string;
+  author: IAuthor;
+  image: {
+    imageUrl: string;
+  };
+  members: string;
+}
+const upComingEvent: IEvent[] = [
+  {
+    id: 1,
+    title: "Holiday trip to Barcelona Spain",
+    author: {
+      fullName: "John Doe",
+      profileImage: {
+        imageUrl:
+          "https://i.ibb.co.com/hFTPRsW0/0de9d1146da18068833210d399cd593e.jpg",
+      },
+    },
+    image: {
+      imageUrl:
+        "https://i.ibb.co.com/6RknjBzS/35b873adc5c444a2d66ee10e62d473d6.jpg",
+    },
+    members: "1.1K",
+  },
+  {
+    id: 2,
+    title: "Holiday trip to Barcelona Spain",
+    author: {
+      fullName: "John Doe",
+      profileImage: {
+        imageUrl:
+          "https://i.ibb.co.com/hFTPRsW0/0de9d1146da18068833210d399cd593e.jpg",
+      },
+    },
+    image: {
+      imageUrl:
+        "https://i.ibb.co.com/hFVGYMF0/2588a7b47b42d6dddfdfa08bb9300d00.jpg",
+    },
+    members: "1.1K",
+  },
+  {
+    id: 3,
+    title: "Holiday trip to Barcelona Spain",
+    author: {
+      fullName: "John Doe",
+      profileImage: {
+        imageUrl:
+          "https://i.ibb.co.com/hFTPRsW0/0de9d1146da18068833210d399cd593e.jpg",
+      },
+    },
+    image: {
+      imageUrl:
+        "https://i.ibb.co.com/JVfh3WK/36fe2823b98504660e2f44dc3c1ffb97.jpg",
+    },
+    members: "1.1K",
+  }
+];
+const UpcomingEvents = () => {
+  return (
+    <section className="w-full pt-2 pb-7 border-b border-gray-300">
+      {/* Header Section */}
+      <div className="w-full flex items-center justify-between mb-6">
+        <h1 className="text-xl md:text-2xl font-semibold text-gray-800">
+        Upcoming Tour
+        </h1>
+        <button className="text-primary hover:underline">Show more</button>
+      </div>
+
+      {/* Cards Section */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {upComingEvent.map((event) => (
+          <UpcomingEventCard key={event.id} event={event} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default UpcomingEvents;
