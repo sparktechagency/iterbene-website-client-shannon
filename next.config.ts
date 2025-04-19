08a056fc8b60b1a76bcfd2f830f77160835f8d7c
Md Rakib Ali <rakib2020.tkg@gmail.com>
2025-04-19 10:40:36 +0600

    feat: Implement middleware for redirecting to home and update feed layout with component imports

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  redirects: async () => [
+    {
+      source: "/",
+      destination: "/feed",
+      permanent: true,
+    },
+  ],
   images: {
     domains: ["i.ibb.co.com","images.unsplash.com","randomuser.me","i.postimg.cc"],
   }
--- src/app/(main-layout)/feed/page.tsx
+++ src/app/(main-layout)/feed/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client";
+import ContactList from "@/components/pages/home/contact-list/contacts-list";
+import CreatePost from "@/components/pages/home/create-post/create-post";
+import FriendRequest from "@/components/pages/home/friend-request/friend-request";
+import Posts from "@/components/pages/home/posts/posts";
+import Stories from "@/components/pages/home/stories/stories";
 
-const page = () => {
+
+
+const Feed = () => {
   return (
-    <div>page</div>
-  )
-}
+    <section className="w-full grid grid-cols-1 md:grid-cols-12 gap-5 pr-5">
+        <div className="w-full md:col-span-8 space-y-5 md:space-y-8 overflow-y-auto">
+          <Stories />
+          <CreatePost />
+          <Posts/>
+        </div>
+        <div className="w-full max-w-[382px] mx-auto col-span-full md:col-span-4 space-y-5 md:space-y-8">
+          <FriendRequest />
+          <ContactList />
+        </div>
+    </section>
+  );
+};
 
-export default page
+export default Feed;
