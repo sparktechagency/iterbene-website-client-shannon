b4e0f64e50fa51fbe54144dd194a21655fa65957
rakibislam2233 <rakib2020.tkg@gmail.com>
2025-07-10 09:59:27 +0600

    ADD: view image for post content render component

--- src/components/pages/home/posts/post.content-render.tsx
+++ src/components/pages/home/posts/post.content-render.tsx
@@ -8,6 +8,9 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import useWindowSize from "@/hooks/useWindowSize";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import { useState } from "react";
 
 interface PostContentRenderProps {
   data: IMedia[];
@@ -19,271 +22,318 @@ const PostContentRender = ({ data, isVisible }: PostContentRenderProps) => {
   const { width } = useWindowSize();
   const isMobile = width < 768;
 
-  if (isMobile) {
-    return (
-      <Swiper
-        modules={[Navigation, Pagination, Autoplay]}
-        spaceBetween={10}
-        slidesPerView={1}
-        navigation={{ prevEl: null, nextEl: null }}
-        loop
-        pagination={{ clickable: true }}
-        autoplay={{ delay: 3000 }}
-        className="w-full mt-3"
-      >
-        {data.map((media, index) => (
-          <SwiperSlide key={index}>
-            <div className="aspect-square">
-              {media.mediaType === "image" ? (
+  const [open, setOpen] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // Create a list of slides just for images, for the lightbox
+  const imageSlides = data
+    .filter((media) => media.mediaType === "image")
+    .map((media) => ({ src: media.mediaUrl }));
+
+  // This function finds the correct index in the `imageSlides` array
+  const handleImageClick = (clickedMedia: IMedia) => {
+    const imageIndex = imageSlides.findIndex(
+      (slide) => slide.src === clickedMedia.mediaUrl
+    );
+    if (imageIndex !== -1) {
+      setCurrentIndex(imageIndex);
+      setOpen(true);
+    }
+  };
+
+  const renderDesktopLayout = () => {
+    // Single media item
+    if (mediaCount === 1) {
+      const media = data[0];
+      return (
+        <div className="w-full mt-3">
+          {media.mediaType === "image" ? (
+            <Image
+              src={media?.mediaUrl}
+              alt={`Post media`}
+              width={400}
+              height={350}
+              className="w-full object-contain cursor-pointer rounded-md"
+              onClick={() => handleImageClick(media)}
+            />
+          ) : (
+            <VideoCard
+              url={media?.mediaUrl}
+              isVisible={isVisible}
+              className="w-full h-[400px]"
+            />
+          )}
+        </div>
+      );
+    }
+
+    // Two media items
+    if (mediaCount === 2) {
+      return (
+        <div className="grid grid-cols-2 gap-3 mt-3">
+          {data.map((media, index) => (
+            <div key={index} className="aspect-square">
+              {media?.mediaType === "image" ? (
                 <Image
                   src={media?.mediaUrl}
-                  alt={`Post media ${index + 1}`}
+                  alt="Post media"
                   width={400}
                   height={350}
-                  className="w-full h-full object-cover rounded-xl cursor-pointer"
+                  className="w-full h-[350px]  rounded-xl cursor-pointer"
+                  onClick={() => handleImageClick(media)}
                 />
               ) : (
                 <VideoCard
                   url={media?.mediaUrl}
                   isVisible={isVisible}
-                  className="w-full h-full"
+                  className="w-full h-[350px]"
                 />
               )}
             </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    );
-  }
+          ))}
+        </div>
+      );
+    }
 
-  // Single media item
-  if (mediaCount === 1) {
-    const media = data[0];
-    return (
-      <div className="w-full mt-3">
-        {media.mediaType === "image" ? (
-          <Image
-            src={media?.mediaUrl}
-            alt={`Post media`}
-            width={400}
-            height={350}
-            className="w-full object-contain cursor-pointer rounded-md"
-          />
-        ) : (
-          <VideoCard
-            url={media?.mediaUrl}
-            isVisible={isVisible}
-            className="w-full h-[400px]"
-          />
-        )}
-      </div>
-    );
-  }
-
-  // Two media items
-  if (mediaCount === 2) {
-    return (
-      <div className="grid grid-cols-2 gap-3 mt-3">
-        {data.map((media, index) => (
-          <div key={index} className="aspect-square">
-            {media?.mediaType === "image" ? (
+    // Three media items
+    if (mediaCount === 3) {
+      return (
+        <div className="grid grid-cols-2 gap-3 mt-3">
+          <div className="col-span-full">
+            {data[0]?.mediaType === "image" ? (
               <Image
-                src={media?.mediaUrl}
+                src={data[0]?.mediaUrl}
                 alt="Post media"
                 width={400}
                 height={350}
-                className="w-full h-[350px]  rounded-xl cursor-pointer"
+                className="w-full h-[350px] object-cover rounded-xl cursor-pointer"
+                onClick={() => handleImageClick(data[0])}
               />
             ) : (
               <VideoCard
-                url={media?.mediaUrl}
+                url={data[0]?.mediaUrl}
                 isVisible={isVisible}
-                className="w-full h-[350px]"
+                className="h-[350px]"
               />
             )}
           </div>
-        ))}
-      </div>
-    );
-  }
-
-  // Three media items
-  if (mediaCount === 3) {
-    return (
-      <div className="grid grid-cols-2 gap-3 mt-3">
-        <div className="col-span-full">
-          {data[0]?.mediaType === "image" ? (
-            <Image
-              src={data[0]?.mediaUrl}
-              alt="Post media"
-              width={400}
-              height={350}
-              className="w-full h-[350px] object-cover rounded-xl cursor-pointer"
-            />
-          ) : (
-            <VideoCard
-              url={data[0]?.mediaUrl}
-              isVisible={isVisible}
-              className="h-[350px]"
-            />
-          )}
-        </div>
-        <div className="col-span-1">
-          {data[1]?.mediaType === "image" ? (
-            <Image
-              src={data[1]?.mediaUrl}
-              alt="Post media"
-              width={400}
-              height={260}
-              className="w-full h-[260px] object-cover rounded-xl cursor-pointer"
-            />
-          ) : (
-            <VideoCard
-              url={data[1]?.mediaUrl}
-              isVisible={isVisible}
-              className="h-[260px]"
-            />
-          )}
-        </div>
-        <div className="col-span-1">
-          {data[2]?.mediaType === "image" ? (
-            <Image
-              src={data[2]?.mediaUrl}
-              alt="Post media"
-              width={400}
-              height={260}
-              className="w-full h-[260px] object-cover rounded-xl cursor-pointer"
-            />
-          ) : (
-            <VideoCard url={data[2]?.mediaUrl} isVisible={isVisible} />
-          )}
-        </div>
-      </div>
-    );
-  }
-  // Four media items
-  if (mediaCount === 4) {
-    return (
-      <div className="grid grid-cols-3 gap-3 mt-3">
-        <div className="row-span-2 col-span-1">
-          {data[0]?.mediaType === "image" ? (
-            <Image
-              src={data[0]?.mediaUrl}
-              alt="Post media"
-              width={400}
-              height={800}
-              className="w-full h-full object-cover rounded-xl cursor-pointer"
-            />
-          ) : (
-            <VideoCard url={data[0]?.mediaUrl} isVisible={isVisible} />
-          )}
-        </div>
-        <div className="row-span-2 col-span-1">
-          {data[1]?.mediaType === "image" ? (
-            <Image
-              src={data[1]?.mediaUrl}
-              alt="Post media"
-              width={400}
-              height={400}
-              className="w-full h-full object-cover rounded-xl cursor-pointer"
-            />
-          ) : (
-            <VideoCard url={data[1]?.mediaUrl} isVisible={isVisible} />
-          )}
-        </div>
-        <div className="col-span-1">
-          {data[2]?.mediaType === "image" ? (
-            <Image
-              src={data[2]?.mediaUrl}
-              alt="Post media"
-              width={400}
-              height={400}
-              className="w-full h-full object-cover rounded-xl cursor-pointer"
-            />
-          ) : (
-            <VideoCard url={data[2]?.mediaUrl} isVisible={isVisible} />
-          )}
-        </div>
-        <div className="col-span-1">
-          {data[3]?.mediaType === "image" ? (
-            <Image
-              src={data[2]?.mediaUrl}
-              alt="Post media"
-              width={400}
-              height={400}
-              className="w-full h-full object-cover rounded-xl cursor-pointer"
-            />
-          ) : (
-            <VideoCard url={data[2]?.mediaUrl} isVisible={isVisible} />
-          )}
-        </div>
-      </div>
-    );
-  }
-  // Four media items ++
-  if (mediaCount > 4) {
-    return (
-      <div className="grid grid-cols-3 gap-3 mt-3">
-        <div className="row-span-2 col-span-1">
-          {data[0]?.mediaType === "image" ? (
-            <Image
-              src={data[0]?.mediaUrl}
-              alt="Post media"
-              width={400}
-              height={400}
-              className="w-full h-full object-cover rounded-xl cursor-pointer"
-            />
-          ) : (
-            <VideoCard url={data[0]?.mediaUrl} isVisible={isVisible} />
-          )}
-        </div>
-        <div className="row-span-2 col-span-1">
-          {data[1]?.mediaType === "image" ? (
-            <Image
-              src={data[1]?.mediaUrl}
-              alt="Post media"
-              width={400}
-              height={400}
-              className="w-full h-full object-cover rounded-xl cursor-pointer"
-            />
-          ) : (
-            <VideoCard url={data[1]?.mediaUrl} isVisible={isVisible} />
-          )}
-        </div>
-        <div className="col-span-1">
-          {data[2]?.mediaType === "image" ? (
-            <Image
-              src={data[2]?.mediaUrl}
-              alt="Post media"
-              width={400}
-              height={400}
-              className="w-full h-full object-cover rounded-xl cursor-pointer"
-            />
-          ) : (
-            <VideoCard url={data[2]?.mediaUrl} isVisible={isVisible} />
-          )}
-        </div>
-        <div className="col-span-1 relative">
-          {data[3]?.mediaType === "image" ? (
-            <Image
-              src={data[2]?.mediaUrl}
-              alt="Post media"
-              width={400}
-              height={400}
-              className="w-full h-full object-cover rounded-xl cursor-pointer"
-            />
-          ) : (
-            <VideoCard url={data[2]?.mediaUrl} isVisible={isVisible} />
-          )}
-          <div className="absolute top-0 left-0 w-full h-full bg-black/30 rounded-xl">
-            <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-xl md:text-3xl">
-              {mediaCount}+
-            </span>
+          <div className="col-span-1">
+            {data[1]?.mediaType === "image" ? (
+              <Image
+                src={data[1]?.mediaUrl}
+                alt="Post media"
+                width={400}
+                height={260}
+                className="w-full h-[260px] object-cover rounded-xl cursor-pointer"
+                onClick={() => handleImageClick(data[1])}
+              />
+            ) : (
+              <VideoCard
+                url={data[1]?.mediaUrl}
+                isVisible={isVisible}
+                className="h-[260px]"
+              />
+            )}
+          </div>
+          <div className="col-span-1">
+            {data[2]?.mediaType === "image" ? (
+              <Image
+                src={data[2]?.mediaUrl}
+                alt="Post media"
+                width={400}
+                height={260}
+                className="w-full h-[260px] object-cover rounded-xl cursor-pointer"
+                onClick={() => handleImageClick(data[2])}
+              />
+            ) : (
+              <VideoCard url={data[2]?.mediaUrl} isVisible={isVisible} />
+            )}
           </div>
         </div>
-      </div>
-    );
-  }
+      );
+    }
+    // Four media items
+    if (mediaCount === 4) {
+      return (
+        <div className="grid grid-cols-3 gap-3 mt-3">
+          <div className="row-span-2 col-span-1">
+            {data[0]?.mediaType === "image" ? (
+              <Image
+                src={data[0]?.mediaUrl}
+                alt="Post media"
+                width={400}
+                height={800}
+                className="w-full h-full object-cover rounded-xl cursor-pointer"
+                onClick={() => handleImageClick(data[0])}
+              />
+            ) : (
+              <VideoCard url={data[0]?.mediaUrl} isVisible={isVisible} />
+            )}
+          </div>
+          <div className="row-span-2 col-span-1">
+            {data[1]?.mediaType === "image" ? (
+              <Image
+                src={data[1]?.mediaUrl}
+                alt="Post media"
+                width={400}
+                height={400}
+                className="w-full h-full object-cover rounded-xl cursor-pointer"
+                onClick={() => handleImageClick(data[1])}
+              />
+            ) : (
+              <VideoCard url={data[1]?.mediaUrl} isVisible={isVisible} />
+            )}
+          </div>
+          <div className="col-span-1">
+            {data[2]?.mediaType === "image" ? (
+              <Image
+                src={data[2]?.mediaUrl}
+                alt="Post media"
+                width={400}
+                height={400}
+                className="w-full h-full object-cover rounded-xl cursor-pointer"
+                onClick={() => handleImageClick(data[2])}
+              />
+            ) : (
+              <VideoCard url={data[2]?.mediaUrl} isVisible={isVisible} />
+            )}
+          </div>
+          <div className="col-span-1">
+            {data[3]?.mediaType === "image" ? (
+              <Image
+                src={data[3]?.mediaUrl}
+                alt="Post media"
+                width={400}
+                height={400}
+                className="w-full h-full object-cover rounded-xl cursor-pointer"
+                onClick={() => handleImageClick(data[3])}
+              />
+            ) : (
+              <VideoCard url={data[3]?.mediaUrl} isVisible={isVisible} />
+            )}
+          </div>
+        </div>
+      );
+    }
+    // Four media items ++
+    if (mediaCount > 4) {
+      return (
+        <div className="grid grid-cols-3 gap-3 mt-3">
+          <div className="row-span-2 col-span-1">
+            {data[0]?.mediaType === "image" ? (
+              <Image
+                src={data[0]?.mediaUrl}
+                alt="Post media"
+                width={400}
+                height={400}
+                className="w-full h-full object-cover rounded-xl cursor-pointer"
+                onClick={() => handleImageClick(data[0])}
+              />
+            ) : (
+              <VideoCard url={data[0]?.mediaUrl} isVisible={isVisible} />
+            )}
+          </div>
+          <div className="row-span-2 col-span-1">
+            {data[1]?.mediaType === "image" ? (
+              <Image
+                src={data[1]?.mediaUrl}
+                alt="Post media"
+                width={400}
+                height={400}
+                className="w-full h-full object-cover rounded-xl cursor-pointer"
+                onClick={() => handleImageClick(data[1])}
+              />
+            ) : (
+              <VideoCard url={data[1]?.mediaUrl} isVisible={isVisible} />
+            )}
+          </div>
+          <div className="col-span-1">
+            {data[2]?.mediaType === "image" ? (
+              <Image
+                src={data[2]?.mediaUrl}
+                alt="Post media"
+                width={400}
+                height={400}
+                className="w-full h-full object-cover rounded-xl cursor-pointer"
+                onClick={() => handleImageClick(data[2])}
+              />
+            ) : (
+              <VideoCard url={data[2]?.mediaUrl} isVisible={isVisible} />
+            )}
+          </div>
+          <div
+            className="col-span-1 relative"
+            onClick={() => handleImageClick(data[3])}
+          >
+            {data[3]?.mediaType === "image" ? (
+              <Image
+                src={data[3]?.mediaUrl}
+                alt="Post media"
+                width={400}
+                height={400}
+                className="w-full h-full object-cover rounded-xl cursor-pointer"
+              />
+            ) : (
+              <VideoCard url={data[3]?.mediaUrl} isVisible={isVisible} />
+            )}
+            <div className="absolute top-0 left-0 w-full h-full bg-black/30 rounded-xl">
+              <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-xl md:text-3xl">
+                {mediaCount - 3}+
+              </span>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <>
+      {isMobile ? (
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
+          spaceBetween={10}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          autoplay={{ delay: 3000 }}
+          className="w-full mt-3"
+        >
+          {data.map((media, index) => (
+            <SwiperSlide key={index}>
+              <div className="aspect-square">
+                {media.mediaType === "image" ? (
+                  <Image
+                    src={media?.mediaUrl}
+                    alt={`Post media ${index + 1}`}
+                    width={400}
+                    height={350}
+                    className="w-full h-full object-cover rounded-xl cursor-pointer"
+                    onClick={() => handleImageClick(media)}
+                  />
+                ) : (
+                  <VideoCard
+                    url={media?.mediaUrl}
+                    isVisible={isVisible}
+                    className="w-full h-full"
+                  />
+                )}
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : (
+        renderDesktopLayout()
+      )}
+      <Lightbox
+        open={open}
+        close={() => setOpen(false)}
+        slides={imageSlides}
+        index={currentIndex}
+      />
+    </>
+  );
 };
 
 export default PostContentRender;
